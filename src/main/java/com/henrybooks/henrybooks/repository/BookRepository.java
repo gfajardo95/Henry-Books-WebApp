47e2c2d595ef90ed38b91dc2d38a232eed6409af
Gabriel Fajardo <gfajardo95@gmail.com>
2017-12-03 19:35:14 -0500

    create repos that implement spring's jparepository interface

---
+++ src/main/java/com/henrybooks/henrybooks/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.henrybooks.henrybooks.repository;
+
+import com.henrybooks.henrybooks.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, String> {
+}
