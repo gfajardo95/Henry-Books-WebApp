86d615e2dadb094519a9f22be7ee0c81b8a00055
Gabriel Fajardo <gfajardo95@gmail.com>
2017-12-03 22:00:08 -0500

    implement custom delete method with @Query annotation

--- src/main/java/com/henrybooks/henrybooks/repository/BookRepository.java
+++ src/main/java/com/henrybooks/henrybooks/repository/BookRepository.java
@@ -2,8 +2,19 @@ package com.henrybooks.henrybooks.repository;
 
 import com.henrybooks.henrybooks.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, String> {
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM book " +
+            "WHERE book.bookCode = :id", nativeQuery = true)
+    void deleteOne(@Param("id") String id);
 }
